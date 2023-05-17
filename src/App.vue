6ad3eeed4165d264ffc90d27b3c45866ae4142e2
mhdrmaulana <129238875+mhdrmaulana@users.noreply.github.com>
2023-05-17 11:57:10 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -21,8 +21,8 @@ import HelloWorld from './components/HelloWorld.vue'
           <a class="nav-link" href="/about">Keutamaan Membaca Al-Qur'an</a>
         </li>
         <form class="d-flex" role="search">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success" type="submit">Search</button>
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" style="background-color: white;" v-model="Search" @input="">
+      <button class="btn btn-outline-success" type="submit" style="background-color: white; color: black;">Search</button>
     </form>
       </ul>
       
